3563464bf82c5882e1ea9b0f8f1e1f14ca0c1c19
mohammedfares3 <150060209+mohammedfares3@users.noreply.github.com>
2023-12-19 02:56:09 +0300

    Update data_loader.py

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -1,1 +1,33 @@
+# -*- coding: utf-8 -*-
 
+#Описание:
+    #Функция загружает данные из указанного файла.
+
+    #Аргументы:
+    #- fname (str): Имя файла, из которого нужно загрузить данные.
+
+    #Возвращает:
+    #- list: Список строк, содержащих данные из файла.
+
+    #Примечание:
+    #Если файл не найден, функция возвращает пустой список.
+
+
+def load_data_from_file(fname: str) -> list:
+    
+    try:
+        with open(fname, 'r') as file:
+            lines = file.readlines()
+            data = []
+            for line in lines:
+                numbers = line.split()  # Разделяем строку на числа
+                for num in numbers:
+                    # Преобразуем каждое число в float и добавляем в список
+                    data.append(float(num))
+            return data
+    except FileNotFoundError:
+        print(f"File '{fname}' not found.")
+        return []
+    except ValueError as e:
+        print(f"Error reading numeric values ​​from file '{fname}': {e}")
+        return []
