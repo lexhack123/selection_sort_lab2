97ca2620863ec278e1bafbc5e9eaa6aac50223a4
lexhack123 <129697363+lexhack123@users.noreply.github.com>
2023-12-19 05:45:46 +0300

    Update plot_saver.py

--- modules/plot_saver.py
+++ modules/plot_saver.py
@@ -1,1 +1,20 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
 
+ #Описание:
+    #Функция строит график для отсортированных данных.
+    #Аргументы:
+    #- sorted_data (list): Отсортированный список данных.
+
+def save_plot(data: list, fname: str):
+    
+    try:
+        plt.plot(data)
+        plt.title('Sorted data')
+        plt.xlabel('Index')
+        plt.ylabel('Meaning')
+        plt.savefig(fname)
+        plt.close()
+        print(f"The graph is saved to file '{fname}'.")
+    except Exception as e:
+        print(f"Error while plotting: {e}")
